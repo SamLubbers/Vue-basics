974079d501eee4d72260028044cdd26fd2ed87aa
Sam~Lubbers <lubbers.sc@gmail.com>
2018-02-28 07:16:15 +0000

    game layout

--- routes/exercises.js
+++ routes/exercises.js
@@ -2,22 +2,26 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/1', (req, res) => {
-  res.render('exercises/1');
+	res.render('exercises/1');
 });
 
 router.get('/2', (req, res) => {
-  res.render('exercises/2');
+	res.render('exercises/2');
 });
 
 router.get('/3', (req, res) => {
-  res.render('exercises/3');
+	res.render('exercises/3');
 });
 
 router.get('/4', (req, res) => {
-  res.render('exercises/4');
+	res.render('exercises/4');
 });
 
 router.get('/5', (req, res) => {
-  res.render('exercises/5');
+	res.render('exercises/5');
+});
+
+router.get('/game', (req, res) => {
+	res.render('exercises/game');
 });
 module.exports = router;
