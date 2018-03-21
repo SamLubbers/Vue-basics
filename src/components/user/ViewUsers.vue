9cf447edbbae5f17c66da3c4ca76338c4b755cab
SamLubbers <lubbers.sc@gmail.com>
2018-03-21 09:30:10 +0000

    view users component

---
+++ src/components/user/ViewUsers.vue
@@ -0,0 +1,36 @@
+<template lang="html">
+  <div>
+    <template v-if='users.length > 0'>
+    <table class="table">
+      <thead>
+        <tr>
+          <th v-for='(v, k) in users[0]' :key='k'>k</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for='(user, index) in users' :key='index'>
+          <td v-for='(v, k, i) in user' :key='i'>v</td>
+        </tr>
+      </tbody>
+    </table>
+    </template>
+    <template v-else>
+      <div class="center">
+        <p>No users currently in the database. Try adding a user</p>
+      </div>
+    </template>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      'users': []
+    }
+  }
+}
+</script>
+
+<style lang="sass" scoped>
+</style>
--- src/examples/Users.vue
+++ src/examples/Users.vue
@@ -3,30 +3,38 @@
     <div class="row">
       <div class="col-12 navigation">
         <div class="flex-row" style="justify-content: space-evenly">
-          <button class="btn btn-outline-dark" @click="state='view'">View Users</button>
-          <button class="btn btn-outline-dark" @click="state='new'">New User</button>
-          <button class="btn btn-outline-dark" @click="state='delete'">Delete User</button>
+          <button class="btn btn-outline-dark" @click="state='viewusers'">View Users</button>
+          <button class="btn btn-outline-dark" @click="state='newuser'">New User</button>
+          <button class="btn btn-outline-dark" @click="state='deleteuser'">Delete User</button>
         </div>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-12">
+        <keep-alive>
+          <component :is='state'/>
+        </keep-alive>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import ViewUsers from '@/components/user/ViewUsers'
+
 export default {
+  components: {
+    viewusers: ViewUsers
+  },
   data () {
     return {
-      state: 'view'
+      state: 'viewusers'
     }
   }
 }
 </script>
 
 <style lang="sass" scoped>
-body
-  background: #212529
-  color: white
-
 .navigation
   border: 1px solid #343a40
   border-right: 0
