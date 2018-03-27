ec9186fb6dcfb73848e6388b87ef48349f3419b4
SamLubbers <lubbers.sc@gmail.com>
2018-03-27 18:14:59 +0100

    add transition to vue router change

--- src/examples/Users.vue
+++ src/examples/Users.vue
@@ -12,7 +12,9 @@
     </div>
     <div class="row">
       <div class="col-12">
-          <router-view></router-view>
+        <transition name='fade' mode='out-in'>
+            <router-view></router-view>
+        </transition>
       </div>
     </div>
     </template>
