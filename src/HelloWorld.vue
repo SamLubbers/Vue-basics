268c5705d551ec4a4601f0e05cf2e77677d05c62
SamLubbers <lubbers.sc@gmail.com>
2018-03-27 08:56:47 +0100

    routes to all page components

--- src/HelloWorld.vue
+++ src/HelloWorld.vue
@@ -1,12 +1,17 @@
 <template>
   <div class="container">
     <div class="row">
-      <div class="col-4">
+      <div class="col-12 col-sm-6">
         <h1>Vue.js basics</h1>
-        <p>{{description}}</p>
-        <hr>
-        <p>Update the description from this input field</p>
-        <input type="text" v-model="description">
+        <div style="display: flex; flex-direction: column">
+          <button class="btn btn-primary vertical-margin" @click='$router.push("/examples/Events")'>Events</button>
+          <button class="btn btn-primary vertical-margin" @click='$router.push("/examples/DynamicLink")'>Dynamic Link</button>
+          <button class="btn btn-primary vertical-margin" @click='$router.push("/examples/DynamicTable")'>Dynamic Table</button>
+          <button class="btn btn-primary vertical-margin" @click='$router.push("/examples/DynamicStyling")'>Dynamic Styling</button>
+          <button class="btn btn-primary vertical-margin" @click='$router.push("/exercises/Game")'>Game</button>
+          <button class="btn btn-primary vertical-margin" @click='$router.push("/exercises/MathQuiz")'>Math Quiz</button>
+          <button class="btn btn-primary vertical-margin" @click='$router.push("/exercises/Quotes")'>Quotes</button>
+        </div>
       </div>
     </div>
   </div>
@@ -22,3 +27,7 @@ export default {
   }
 }
 </script>
+<style lang='sass' scoped>
+.vertical-margin
+  margin: 1rem 0
+</style>
