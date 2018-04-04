356ea74b3254f9019115b78bade392d5830952ae
SamLubbers <lubbers.sc@gmail.com>
2018-04-04 07:16:09 +0100

    stocktrader app navbar

--- src/exercises/StockTrader.vue
+++ src/exercises/StockTrader.vue
@@ -1,23 +1,35 @@
 <template>
-  <div class="container">
-    <div class="row mb-3">
-      <div class="col-12" style="border: 1px solid black">
-        <router-link :to="{ name: 'myStocks' }">Portfolio</router-link>
-        <router-link :to="{ name: 'allStocks' }">Stocks</router-link>
+  <div>
+    <nav class="navbar navbar-expand-md navbar-light bg-light mb-3">
+      <router-link tag='a' class="navbar-brand" :to="{ name: 'traderHome' }" active-class='active'>Stock Trader</router-link>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapseNav" aria-controls="collapseNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="collapseNav">
+        <div class="navbar-nav mr-auto">
+            <router-link tag='a' class="nav-item nav-link" :to="{ name: 'myStocks' }" active-class='active'>Portfolio</router-link>
+            <router-link tag='a' class="nav-item nav-link" :to="{ name: 'allStocks' }" active-class='active'>Stocks</router-link>
+        </div>
+        <span class="navbar-text" style="font-weight: bold; color: black">Funds: {{myFunds}}$</span>
       </div>
-    </div>
+    </nav>
+    <div class="container">
     <div class="row">
       <div class="col-12 full-width">
         <router-view></router-view>
       </div>
     </div>
   </div>
+  </div>
 </template>
 <script>
+import { mapGetters } from 'vuex'
 export default {
-  data: () => ({
-
-  })
+  computed: {
+    ...mapGetters('StockTrader', [
+      'myFunds'
+    ])
+  }
 }
 </script>
 <style lang="sass" scoped>
